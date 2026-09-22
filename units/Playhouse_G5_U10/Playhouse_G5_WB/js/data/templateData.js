fcda1dd235628b6dceeea9dbd65be4053db5bb93
duha1dahamsheh <developer2@bytechjo.com>
2026-09-22 11:09:03 +0300

    update and fix buges unit 10 student book

--- units/Playhouse_G5_U10/Playhouse_G5_WB/js/data/templateData.js
+++ units/Playhouse_G5_U10/Playhouse_G5_WB/js/data/templateData.js
@@ -7,7 +7,7 @@ var _data = {
         // "bgimage": "./images/AGW_1_SB_U15_header.png",
         "bgcolor": "rgb(212,239,253)",
         "icon": "./images/unit-icon.png",       
-        "audio": "./audios/p49/Unit_10_It’s_Delicious!.mp3",
+        "audio": "./audios/P49/Unit_10_It’s_Delicious!.mp3",
         "title":{
             "text": "It’s Delicious!",
             "color":"rgb(255, 255, 255);",
